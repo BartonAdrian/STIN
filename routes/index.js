6373ef8998d030a6cfb91a6cdc9e566f4189917a
BartonAdrian <adrian.barton@tul.cz>
2022-04-24 09:31:58 +0200

    New initial commit

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express=require('express')
-const router = express.Router()
-
-router.get('/',(req,res)=>{
-    res.send('Hi, my name is Saffrona. How can I help you ?')
-})
-
-module.exports=router
--- server.js
+++ server.js
@@ -1,15 +1,50 @@
-const express = require('express')
-const app = express()
-const expressLayouts= require('express-ejs-layouts')
+const express = require('express');
+const path = require('path');
+const app = express();
+let cookieParser = require('cookie-parser');
+var bodyparser=require('body-parser');
+var session=require('express-session');
 
-const indexRouter=require('./routes/index')
+const name="Shfaron";
 
-app.set('view engine','ejs')
-app.set('views',__dirname+'/views')
-app.set('layout','layouts/layout')
-app.use(expressLayouts)
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname+"/public")));
 
-app.use('/',indexRouter)
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+secret:'secret',
+resave:false,
+saveUninitialized:false,
+cookie:{
+maxAge:10000
+}}));
+ 
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+ 
+app.post('/', (req, res) => {
+    const { message } = req.headers;
+    const response=checkQuestion(message);
+    console.log(req.cookies);
+    //console.log(message);
+    let conversation={
+        question: message,
+        answer: response,
+    }
+    res.cookie('cookieName',{conversation});
+    res.send({
+      name,
+      response,
+    });
+});
 
-app.listen(process.env.PORT || 3000)
+function checkQuestion(msg){
+    return "odpoved";
+}
+
+
+
+app.listen(3000, () => {
+    console.log('Our express server is up on port 3000');
+  });
